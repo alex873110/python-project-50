db8fce2427f6904dd402714bbd4ab5fe75ec1c84
Alexander <alexzhur87@mail.ru>
2023-06-07 11:19:58 +0300

    add -f and --format to gendiff

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -7,6 +7,7 @@ def main():
     parser = argparse.ArgumentParser(description='Compares two configuration files and shows a difference.')
     parser.add_argument('first_file', type=str, help='')
     parser.add_argument('second_file', type=str, help='')
+    parser.add_argument('-f', '--format', type=str, help='set format of output')
     args = parser.parse_args()
     print(args.first_file)
 
