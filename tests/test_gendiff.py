add4250b3e196ddd31f4c75b5a858f39ad235a11
Alexander <alexzhur87@mail.ru>
2023-06-11 12:41:22 +0300

    refactoring test_gendiff.py

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,6 @@
 from gendiff.generate_diff  import generate_diff
 
+result_1 = "{\n- follow: false\nhost: hexlet.io\n- proxy: 123.234.53.22\n
+              - timeout: 50\n+ timeout: 20\n+ verbose: true\n}"
 def test_generate_diff():
-    assert generate_diff(gendiff/files/file1.json, gendiff/files/file2.json) == "{\n
-                         - follow: false\nhost: hexlet.io\n- proxy: 123.234.53.22\n
-                         - timeout: 50\n+ timeout: 20\n+ verbose: true\n}"
+    assert generate_diff(gendiff/files/file1.json, gendiff/files/file2.json) == result_1
