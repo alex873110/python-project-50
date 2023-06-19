40136389fa72450f2f5108e809174a6d10001001
Alexander <alexzhur87@mail.ru>
2023-06-20 00:57:56 +0300

    refactoring generate_diff.py for deepfile(deepdictionary) accept

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,32 +1,65 @@
 
-def change_bool(dict):
-    for key, value in dict.items():
-        if type(value) == bool:
+def change_bool(dictionary):
+    for key, value in dictionary.items():
+        if isinstance(value, dict):
+            change_bool(value)
+        elif isinstance(value, bool):
             if value is True:
-                dict[key] = 'true'
+                dictionary[key] = 'true'
             elif value is False:
-                dict[key] = 'false'
-    return dict
+                dictionary[key] = 'false'
+        elif value is None:
+            dictionary[key] = 'null'
+    return dictionary
 
 
-def generate_diff(parsed):
-    file1, file2 = parsed
-    dict1 = dict(sorted(file1.items()))
-    dict1 = change_bool(dict1)
-    dict2 = dict(sorted(file2.items()))
-    dict2 = change_bool(dict2)
-    result = ''
-    for key in dict1:
-        if key in dict2:
-            if dict1[key] == dict2[key]:
-                result += f'  {key}: {dict1[key]}\n'
-            else:
-                result += f'- {key}: {dict1[key]}\n'
-                result += f'+ {key}: {dict2[key]}\n'
-        else:
-            result += f'- {key}: {dict1[key]}\n'
-    for key2 in dict2:
-        if key2 not in dict1:
-            result += f'+ {key2}: {dict2[key2]}\n'
-    result = '{\n' + result + '}'
-    return result
+def diff(data_1, data_2):
+    new_data = {}
+    keys = list(data_1.keys() | data_2.keys())
+    keys.sort()
+    for key in keys:
+        if key in data_1 and key in data_2:
+            if isinstance(data_1[key], dict) and isinstance(data_2[key], dict):
+                # in case no diff between keys, add two blanks to key,
+                # if additional data in second file, add '+' and blank to key,
+                # if data deleted from first file, add '-' and blank to key
+                new_data[f'  {key}'] = diff(data_1[key], data_2[key])
+            elif data_1[key] == data_2[key]:
+                new_data[f'  {key}'] = data_1[key]
+            elif data_1[key] != data_2[key]:
+                new_data[f'- {key}'] = data_1[key]
+                new_data[f'+ {key}'] = data_2[key]
+        elif key in data_1:
+            new_data[f'- {key}'] = data_1[key]
+        elif key in data_2:
+            new_data[f'+ {key}'] = data_2[key]
+    return new_data
+
+
+def convert_to_stroke(data):
+    change_bool(data)
+
+    def walk(data, level):
+        result = ''
+        symbol = ('    ' * level)[:-2]
+        last_blanks = ('    ' * level)
+        if isinstance(data, dict):
+            for key in data.keys():
+                marks = ['+', '-', ' ']
+                if key[0] not in marks:
+                    symbol = ('    ' * level)
+                value = data[key]
+                if isinstance(data[key], dict):
+                    result += f'\n{symbol}{key}: '
+                    result += f'{{{walk(value, (level + 1))}\n{last_blanks}}}'
+                else:
+                    result += f'\n{symbol}{key}:'
+                    if value:
+                        result += f' {value}'
+        return result
+    return f'{{{walk(data, 1)}\n}}'
+
+
+def generate_diff(converted):
+    file1_data, file2_data = converted
+    return convert_to_stroke(diff(file1_data, file2_data))
