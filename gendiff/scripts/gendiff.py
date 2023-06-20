874070718dcf43e06480ac4b46341df66418b715
Alexander <alexzhur87@mail.ru>
2023-06-20 19:13:40 +0300

    refactoring gendiff.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -12,10 +12,12 @@ def main():
     parser.add_argument('first_file', type=str, help='')
     parser.add_argument('second_file', type=str, help='')
     parser.add_argument('-f', '--format', type=str,
-                        default=stylish, help='set format of output')
+                        default='stylish', help='set format of output')
     args = parser.parse_args()
     converted = convert(args.first_file, args.second_file)
-    print(generate_diff(converted, args.format))
+    if args.format == "stylish":
+        formater = stylish
+    print(generate_diff(converted, formater))
 
 
 if __name__ == "__main__":
