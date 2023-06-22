88b052e0d8272ee1618f1a0f58822830ffd776e3
Alexander <alexzhur87@mail.ru>
2023-06-22 23:10:46 +0300

    refactoring stylish.py for reduce complexity

--- gendiff/formaters/stylish.py
+++ gendiff/formaters/stylish.py
@@ -9,25 +9,37 @@ def change_bool(dictionary):
     return dictionary
 
 
+def generate_stroke(key, value, level):
+    mark_size = 2
+    marks = ['+', '-', ' ']
+    if key[0] not in marks:
+        mark_size = 0
+    significant = ' '
+    # according to task for each level indent = 4 symbols
+    symbols_quantity = 4 * level - mark_size
+    symbols = symbols_quantity * significant
+    if isinstance(value, dict):
+        stroke = f'\n{symbols}{key}:'
+    else:
+        stroke = f'\n{symbols}{key}:'
+        if value:
+            stroke += f' {value}'
+    return stroke
+
+
 def make_volume_string(data):
     change_bool(data)
 
     def walk(data, level):
         result = ''
-        symbol = ('    ' * level)[:-2]
         last_blanks = ('    ' * level)
-        if isinstance(data, dict):
-            for key in data.keys():
-                marks = ['+', '-', ' ']
-                if key[0] not in marks:
-                    symbol = ('    ' * level)
-                value = data[key]
-                if isinstance(data[key], dict):
-                    result += f'\n{symbol}{key}: '
-                    result += f'{{{walk(value, (level + 1))}\n{last_blanks}}}'
-                else:
-                    result += f'\n{symbol}{key}:'
-                    if value:
-                        result += f' {value}'
+        for key in data.keys():
+            value = data[key]
+            if isinstance(data[key], dict):
+                result += generate_stroke(key, value, level)
+                result += f' {{{walk(value, (level + 1))}\n{last_blanks}}}'
+            else:
+                result += generate_stroke(key, value, level)
         return result
+
     return f'{{{walk(data, 1)}\n}}'
