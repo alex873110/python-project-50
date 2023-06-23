76f2278c9b890b877cf557d91ad538c5ea5c58d8
Alexander <alexzhur87@mail.ru>
2023-06-23 10:21:38 +0300

    refactoring generate_diff.py for reduce complexity

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,26 +1,36 @@
 from gendiff.formaters.stylish import make_volume_string
 
 
+def mark_key(data1, data2, key):
+    keys = list(data1.keys() | data2.keys())
+    only_in_data1_keys = list(data1.keys() - data2.keys())
+    only_in_data2_keys = list(data2.keys() - data1.keys())
+    keys.sort()
+    if key in only_in_data1_keys:
+        new_dict = {f'- {key}': data1[key]}
+    elif key in only_in_data2_keys:
+        new_dict = {f'+ {key}': data2[key]}
+    elif data1[key] == data2[key]:
+        new_dict = {f'  {key}': data1[key]}
+    else:
+        new_dict = {f'- {key}': data1[key], f'+ {key}': data2[key]}
+    return new_dict
+
+
+def is_dicts(dict1, dict2):
+    return (isinstance(dict1, dict) and isinstance(dict2, dict))
+
+
 def diff(data_1, data_2):
     new_data = {}
     keys = list(data_1.keys() | data_2.keys())
+    common_keys = list(data_1.keys() & data_2.keys())
     keys.sort()
     for key in keys:
-        if key in data_1 and key in data_2:
-            if isinstance(data_1[key], dict) and isinstance(data_2[key], dict):
-                # in case no diff between keys, add two blanks to key,
-                # if additional data in second file, add '+' and blank to key,
-                # if data deleted from first file, add '-' and blank to key
-                new_data[f'  {key}'] = diff(data_1[key], data_2[key])
-            elif data_1[key] == data_2[key]:
-                new_data[f'  {key}'] = data_1[key]
-            elif data_1[key] != data_2[key]:
-                new_data[f'- {key}'] = data_1[key]
-                new_data[f'+ {key}'] = data_2[key]
-        elif key in data_1:
-            new_data[f'- {key}'] = data_1[key]
-        elif key in data_2:
-            new_data[f'+ {key}'] = data_2[key]
+        if key in common_keys and is_dicts(data_1[key], data_2[key]):
+            new_data[f'  {key}'] = diff(data_1[key], data_2[key])
+        else:
+            new_data.update(mark_key(data_1, data_2, key))
     return new_data
 
 
