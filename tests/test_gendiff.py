c41db98aac3de1e590fd11e59f2a5a5e35188def
Alexander <alexzhur87@mail.ru>
2023-06-27 10:52:04 +0300

    refactoring test_gendiff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,36 +1,44 @@
 import pytest
 from gendiff.generate_diff import generate_diff
+import os
 
-path1 = './tests/fixtures/file1.json'
-path2 = './tests/fixtures/file2.json'
-path3 = './tests/fixtures/file3.yml'
-path4 = './tests/fixtures/file4.yaml'
-path5 = './tests/fixtures/deepfile1.json'
-path6 = './tests/fixtures/deepfile2.json'
-path7 = './tests/fixtures/deepfile1.yml'
-path8 = './tests/fixtures/deepfile2.yaml'
 
-file = './tests/fixtures/expected.txt'
-deepfile = './tests/fixtures/deepexpected.txt'
-plainfile = './tests/fixtures/plainexpected.txt'
-to_json_file = './tests/fixtures/tojsonexpected.json'
+def get_fixture_path(file_name):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, 'fixtures', file_name)
+
+
+def read(file_path):
+    with open(file_path, 'r') as f:
+        result = f.read()
+    return result
 
 
 @pytest.mark.parametrize('input1,input2,expected',
-                         [(path1, path2, file),
-                          (path3, path4, file),
-                          (path5, path6, deepfile),
-                          (path7, path8, deepfile)])
+                         [('file1.json', 'file2.json', 'expected.txt'),
+                          ('file3.yml', 'file4.yaml', 'expected.txt'),
+                          ('deepfile1.json', 'deepfile2.json',
+                           'deepexpected.txt'),
+                          ('deepfile1.yml', 'deepfile2.yaml',
+                           'deepexpected.txt')])
 def test_generate_diff(input1, input2, expected):
-    result = open(expected, 'r').read()
-    assert generate_diff(input1, input2) + '\n' == result
+    file1 = get_fixture_path(input1)
+    file2 = get_fixture_path(input2)
+    result = read(get_fixture_path(expected))
+    assert generate_diff(file1, file2) + '\n' == result
 
 
-@pytest.mark.parametrize('file1,file2,expected,format',
-                         [(path5, path6, plainfile, 'plain'),
-                          (path7, path8, plainfile, 'plain'),
-                          (path5, path6, to_json_file, 'json'),
-                          (path7, path8, to_json_file, 'json')])
-def test_generate_diff_with_formater(file1, file2, expected, format):
-    result = open(expected, 'r').read()
+@pytest.mark.parametrize('input1,input2,expected,format',
+                         [('deepfile1.json', 'deepfile2.json',
+                           'plainexpected.txt', 'plain'),
+                          ('deepfile1.yml', 'deepfile2.yaml',
+                           'plainexpected.txt', 'plain'),
+                          ('deepfile1.json', 'deepfile2.json',
+                           'tojsonexpected.json', 'json'),
+                          ('deepfile1.yml', 'deepfile2.yaml',
+                           'tojsonexpected.json', 'json')])
+def test_generate_diff_with_formater(input1, input2, expected, format):
+    file1 = get_fixture_path(input1)
+    file2 = get_fixture_path(input2)
+    result = read(get_fixture_path(expected))
     assert generate_diff(file1, file2, format) + '\n' == result
