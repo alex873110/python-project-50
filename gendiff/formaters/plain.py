183ea0c7cea7017c1e0abda1b2d30913a16577c9
Alexander <alexzhur87@mail.ru>
2023-07-07 15:19:42 +0300

    refractoring plain.py

--- gendiff/formaters/plain.py
+++ gendiff/formaters/plain.py
@@ -1,14 +1,14 @@
-from .stylish import change_to_str
 
 
-def replace(something):
-    if isinstance(something, dict):
-        result = '[complex value]'
-    elif isinstance(something, str):
-        result = f"'{something}'"
-    else:
-        result = change_to_str(something)
-    return result
+def modify(data):
+    if isinstance(data, dict):
+        return '[complex value]'
+    elif isinstance(data, str):
+        return f"'{data}'"
+    elif isinstance(data, (int, bool)):
+        return str(data).lower()
+    elif data is None:
+        return 'null'
 
 
 def generate_text(key_, data_, adress_):
@@ -16,12 +16,12 @@ def generate_text(key_, data_, adress_):
     start = f"Property '{adress_}{key_}'"
     text = ''
     if val_['status'] == 'updated':
-        text = f"{start} was updated. From {replace(val_['value']['removed'])}"
-        text += f" to {replace(val_['value']['added'])}\n"
+        text = f"{start} was updated. From {modify(val_['value']['removed'])}"
+        text += f" to {modify(val_['value']['added'])}\n"
     elif val_['status'] == 'removed':
         text = f"{start} was removed\n"
     elif val_['status'] == 'added':
-        text = f"{start} was added with value: {replace(val_['value'])}\n"
+        text = f"{start} was added with value: {modify(val_['value'])}\n"
     return text
 
 
