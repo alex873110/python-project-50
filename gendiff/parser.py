87a97267747e3cecfc3a8894194928f1af5021ee
Alexander <alexzhur87@mail.ru>
2023-07-08 22:03:41 +0300

    refractoring parser.py

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,29 +1,21 @@
 import json
 import yaml
+import os.path
 from yaml.loader import SafeLoader
-
-
-def choice_format(path):
-    extension = str(path)[-4:]
-    if extension == 'json':
-        format = 'json'
-    elif extension == 'yaml' or extension == '.yml':
-        format = 'yaml'
-    else:
-        raise ValueError('This format not supported. '
-                         "Only 'yaml' and 'Json' formats supported")
-    return format
 
 
 def parse_file(file_data, format):
     if format == 'json':
         parsed_data = json.load(file_data)
-    elif format == 'yaml':
+    elif format == 'yaml' or 'yml':
         parsed_data = yaml.load(file_data, Loader=SafeLoader)
+    else:
+        raise ValueError('This format not supported. '
+                         "Only 'yaml' and 'Json' formats supported")
     return parsed_data
 
 
 def open_and_parse(path):
-    format = choice_format(path)
+    format = os.path.splitext(path)[1].strip('.')
     with open(path, 'r') as content:
         return parse_file(content, format)
