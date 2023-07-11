8395c43518bcb957df3bd0ab645a63d9b91dd999
Alexander <alexzhur87@mail.ru>
2023-07-11 23:56:08 +0300

    refractoring  test_gendiff.py, correct tests/fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,7 @@
 import pytest
 from gendiff.generate_diff import generate_diff
 import os
+import json
 
 
 def get_fixture_path(file_name):
@@ -11,16 +12,12 @@ def get_fixture_path(file_name):
 def read(file_path):
     with open(file_path, 'r') as f:
         result = ''.join(f.readlines())
-    return result[:-1]
+    return result
 
 
 @pytest.mark.parametrize('file1_name,file2_name,expected',
-                         [('file1.json', 'file2.json', 'expected'),
-                          ('file1.yml', 'file2.yaml', 'expected'),
-                          ('deepfile1.json', 'deepfile2.json',
-                           'deepexpected.txt'),
-                          ('deepfile1.yml', 'deepfile2.yaml',
-                           'deepexpected.txt')])
+                         [('file1.json', 'file2.json', 'result_stylish'),
+                          ('file1.yml', 'file2.yml', 'result_stylish')])
 def test_generate_diff(file1_name, file2_name, expected):
     file1 = get_fixture_path(file1_name)
     file2 = get_fixture_path(file2_name)
@@ -29,16 +26,26 @@ def test_generate_diff(file1_name, file2_name, expected):
 
 
 @pytest.mark.parametrize('file1_name,file2_name,expected,format',
-                         [('deepfile1.json', 'deepfile2.json',
-                           'plainexpected.txt', 'plain'),
-                          ('deepfile1.yml', 'deepfile2.yaml',
-                           'plainexpected.txt', 'plain'),
-                          ('deepfile1.json', 'deepfile2.json',
-                           'tojsonexpected.json', 'json'),
-                          ('deepfile1.yml', 'deepfile2.yaml',
-                           'tojsonexpected.json', 'json')])
+                         [('file1.json', 'file2.json',
+                           'result_plain', 'plain'),
+                          ('file1.yml', 'file2.yml',
+                           'result_plain', 'plain'),
+                          ('file1.json', 'file2.json',
+                           'result_stylish', 'stylish'),
+                          ('file1.yml', 'file2.yml',
+                           'result_stylish', 'stylish')])
 def test_generate_diff_with_formater(file1_name, file2_name, expected, format):
     file1 = get_fixture_path(file1_name)
     file2 = get_fixture_path(file2_name)
     result = read(get_fixture_path(expected))
     assert generate_diff(file1, file2, format) == result
+
+
+@pytest.mark.parametrize('file1_name,file2_name',
+                         [('file1.json', 'file2.json'),
+                          ('file1.yml', 'file2.yml')])
+def test_generate_diff_json_formater(file1_name, file2_name):
+    file1 = get_fixture_path(file1_name)
+    file2 = get_fixture_path(file2_name)
+    diff = generate_diff(file1, file2, 'json')
+    assert type(json.loads(diff)) == dict
