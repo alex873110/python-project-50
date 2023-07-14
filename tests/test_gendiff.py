dea56c5f67a5e82a128484741514d9ebdcef65b9
Alexander <alexzhur87@mail.ru>
2023-07-14 17:06:30 +0300

    refractoring test_generate_diff

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,7 +1,6 @@
 import pytest
 from gendiff.generate_diff import generate_diff
 import os
-import json
 
 
 def get_fixture_path(file_name):
@@ -15,18 +14,12 @@ def read(file_path):
     return result
 
 
-@pytest.mark.parametrize('file1_name,file2_name,expected',
-                         [('file1.json', 'file2.json', 'result_stylish'),
-                          ('file1.yml', 'file2.yml', 'result_stylish')])
-def test_generate_diff_default_formater(file1_name, file2_name, expected):
-    file1 = get_fixture_path(file1_name)
-    file2 = get_fixture_path(file2_name)
-    result = read(get_fixture_path(expected))
-    assert generate_diff(file1, file2) == result
-
-
 @pytest.mark.parametrize('file1_name,file2_name,expected,format',
                          [('file1.json', 'file2.json',
+                           'result_stylish', ''),
+                          ('file1.yml', 'file2.yml',
+                           'result_stylish', ''),
+                          ('file1.json', 'file2.json',
                            'result_plain', 'plain'),
                           ('file1.yml', 'file2.yml',
                            'result_plain', 'plain'),
@@ -38,14 +31,6 @@ def test_generate_diff_plain_stylish(file1_name, file2_name, expected, format):
     file1 = get_fixture_path(file1_name)
     file2 = get_fixture_path(file2_name)
     result = read(get_fixture_path(expected))
+    if format == '':
+        format = 'stylish'
     assert generate_diff(file1, file2, format) == result
-
-
-@pytest.mark.parametrize('file1_name,file2_name',
-                         [('file1.json', 'file2.json'),
-                          ('file1.yml', 'file2.yml')])
-def test_generate_diff_json_formater(file1_name, file2_name):
-    file1 = get_fixture_path(file1_name)
-    file2 = get_fixture_path(file2_name)
-    diff = generate_diff(file1, file2, 'json')
-    assert type(json.loads(diff)) == dict
